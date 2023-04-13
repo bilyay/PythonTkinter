7e86efdc2b4df87f5187773cddc9a083a1b020f3
bilyay <abd.yay@gmail.com>
2023-04-13 14:47:56 +0300

    tkinter

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Hi")
-input("lklk")
+from tkinter import *
+
+window=Tk()
+
+window.title("First Window Tkinter")
+window.minsize(width=500,height=400)
+label_1=Label(text="I am a label", font=("Arial",16, "bold"))
+label_1.pack()
+label_1.config(text="New TEXT")
+
+
+def button_clicked():
+    label_1.config(text="I got click")
+
+button_1=Button(text="Click Me", command=button_clicked)
+button_1.pack()
+
+window.mainloop()
